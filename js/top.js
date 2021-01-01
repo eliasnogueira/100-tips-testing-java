010ceea34d361a763a36ed0e13d983bad40da048
Elias Nogueira <elias.nogueira@gmail.com>
2021-01-01 18:58:19 +0100

    scroll to top added

---
+++ js/top.js
@@ -0,0 +1,16 @@
+var mybutton = document.getElementById("scrollToTop");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
